e23f73f4f535a87883d18927cbf515aa266fd3ea
Martin nurhidayat <nurhidayatmartin7@gmail.com>
2026-08-09 14:35:40 +0700

    fix secure database

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,10 +1,18 @@
+// ============================================================
+// KONEKSI DATABASE (Aiven MySQL)
+// ============================================================
+
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'mahamate_db',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
