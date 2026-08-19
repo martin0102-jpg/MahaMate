0cff0572fdd69c5f66957ffc293f22d194d13026
Martin nurhidayat <nurhidayatmartin7@gmail.com>
2026-08-19 23:34:44 +0700

    rebuild vercel

--- api/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-    // Testing endpoint
-    if (req.url === '/api/test') {
-        return res.status(200).json({ message: 'Test berhasil!' });
-    }
-    res.status(200).json({ message: 'API is working!' });
-};
